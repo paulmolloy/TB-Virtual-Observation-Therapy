94423338cfdbf294b2bbc3f707963b20b9f1a204
root <root@tb-webserver.us-east1-b.c.winged-metric-231809.internal>
2019-02-19 13:33:36 +0000

    Change file name.

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
