0d04e970e23179b84ec297f4143661fbe21a491b
dormsby <dormsby@tcd.ie>
2019-02-22 02:57:56 +0000

    Progress on tab interface for main menu
    Created 3 tabs which will be allow users to navigate the app by swiping. Still need to implement this on main menu

---
+++ TBVideoJournal/app/src/main/java/ie/tcd/paulm/tbvideojournal/mainmenu/TabDiaryFragment.java
@@ -0,0 +1,17 @@
+package ie.tcd.paulm.tbvideojournal.mainmenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ie.tcd.paulm.tbvideojournal.R;
+
+public class TabDiaryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_diary, container, false);
+    }
+}
