0fbae015e1c21f7804d7d2ae7974e9b00d2b2463
Paul Molloy <molloyp1@tcd.ie>
2019-03-04 11:35:20 +0000

    Delete FFmpeg recording code. Cleanup un-used stuff.

--- TBVideoJournal/app/src/main/java/ie/tcd/paulm/tbvideojournal/opencv/FaceDetector.java
+++ TBVideoJournal/app/src/main/java/ie/tcd/paulm/tbvideojournal/opencv/FaceDetector.java
@@ -2,48 +2,8 @@ package ie.tcd.paulm.tbvideojournal.opencv;
 
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.hardware.display.DisplayManager;
-import android.hardware.display.VirtualDisplay;
-import android.media.MediaRecorder;
-import android.media.projection.MediaProjection;
-import android.media.projection.MediaProjectionManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
+
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
-import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
-import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import org.jcodec.common.io.FileChannelWrapper;
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
@@ -55,19 +15,8 @@ import org.opencv.objdetect.CascadeClassifier;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 
-import ie.tcd.paulm.tbvideojournal.MainActivity;
 import ie.tcd.paulm.tbvideojournal.R;
-import ie.tcd.paulm.tbvideojournal.auth.Auth;
-import ie.tcd.paulm.tbvideojournal.firestore.FSVotVideoRef;
-import ie.tcd.paulm.tbvideojournal.misc.Misc;
-import ie.tcd.paulm.tbvideojournal.steps.PillIntakeSteps;
-
-import static android.app.Activity.RESULT_OK;
 
 public class FaceDetector {
 
@@ -100,7 +49,7 @@ public class FaceDetector {
             // Load the cascade classifier
             cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
         } catch (Exception e) {
-            Log.e("OpenCVActivity", "Error loading cascade", e);
+            Log.e(TAG, "Error loading cascade", e);
         }
 
     }
--- TBVideoJournal/app/src/main/java/ie/tcd/paulm/tbvideojournal/opencv/VotCamera.java
+++ TBVideoJournal/app/src/main/java/ie/tcd/paulm/tbvideojournal/opencv/VotCamera.java
@@ -4,7 +4,6 @@ package ie.tcd.paulm.tbvideojournal.opencv;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.MediaRecorder;
@@ -25,11 +24,6 @@ import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
-import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
-import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -38,17 +32,13 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import org.jcodec.common.io.FileChannelWrapper;
 import org.opencv.android.*;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
-import java.io.FileOutputStream;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -66,28 +56,18 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
     private static final String  TAG              = "FaceCamera";
 
     private CameraBridgeViewBase mOpenCvCameraView;
-    private CascadeClassifier cascadeClassifier;
     private Mat grayscaleImage;
     private Mat colorImage;
-    private Mat rotatedColorImage;
     private Mat grayscaleImageRot;
-    private Mat mRgba;
     private Mat mRgbaF;
     private Mat mRgbaT;
     private FaceDetector faceDetector;
-    private FileChannelWrapper out;
-    private Boolean finished;
-    private FFmpeg ffmpeg;
-    private File dir;
-    private int frameCount;
     // FPS Debugging.
     private int mFPS;
     private long startTime;
     private long currentTime;
     private TextView fpsTextView;
     private static final String VOT_DIR = "/tb-vot/";
-    private static final String VOT_FRAME_PREFIX = "tb-bitmap-frame-";
-    private static final  String VOT_VIDEO_FILENAME = "latest-vot";
     private static final String FIREBASE_VOT_DIR = "vot-videos/";
 
     private static final String FIREBASE_FILENAME = "-vot-";
@@ -151,25 +131,15 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
         File dir = new File (root.getAbsolutePath() + VOT_DIR);
         Log.d(TAG, "Full file path: " + dir.getAbsolutePath());
         dir.mkdirs();
-        frameCount = 0;
-        finished = false;
-
 
         steps.onAllPillsTaken(() -> {
             Misc.toast("All pills taken! (Will add UI for this in a bit)", getContext());
-            finished = true;
-            ffmpeg =  FFmpeg.getInstance(getContext());
-            loadFFMpegBinary();
-            // Magic command for turning the VOT images into an mp4.
-            String[] videoCommand = new String[]{"-i", root.getAbsolutePath() + VOT_DIR
-                    + VOT_FRAME_PREFIX + "%01d.jpg" + "", "-c:v", "libx264", "-c:a", "aac", "-vf",
-                    "setpts=2*PTS, transpose=2", "-pix_fmt", "yuv420p", "-crf", "10", "-r", "15",
-                    "-shortest", "-y", root.getAbsolutePath() + VOT_DIR + VOT_VIDEO_FILENAME + ".mp4"};
-             execFFmpegBinary(videoCommand);
-             stopRecording();
-             recording = false;
-            saveVotFirebase(Environment.getExternalStorageDirectory().getAbsolutePath()
-                    + VOT_DIR + VOT_SCREEN_RECORD_VIDEO_FILENAME + ".mp4");
+            if(recording) {
+                stopRecording();
+                recording = false;
+                saveVotFirebase(Environment.getExternalStorageDirectory().getAbsolutePath()
+                        + VOT_DIR + VOT_SCREEN_RECORD_VIDEO_FILENAME + ".mp4");
+            }
 
         });
         steps.onStepChanged((step, pill) -> {
@@ -197,10 +167,8 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
     @Override
     public void onCameraViewStarted(int width, int height) {
         grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
         mRgbaF = new Mat(height, width, CvType.CV_8UC4);
         mRgbaT = new Mat(width, width, CvType.CV_8UC4);
-        rotatedColorImage = new Mat(width, width, CvType.CV_8UC4);
         grayscaleImageRot= new Mat(width, width, CvType.CV_8UC4);
         faceDetector = new FaceDetector(getContext(), height);
     }
@@ -227,15 +195,6 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
 
         // Use the classifier to detect faces
         colorImage = faceDetector.process(colorImage, grayscaleImageRot);
-
-            // Un-comment this to enable the Ffmpeg video saving again.
-//        // Save the frame to be used for the video
-//        if( !finished) {
-//            saveTempBitmapFrame(matToBitmap(colorImage), frameCount);
-//            Log.d(TAG, "Bitmap added to list");
-//            frameCount ++;
-//        }
-
 
         // Keep track of what the FPS is.
         getActivity().runOnUiThread(new Runnable() {
@@ -268,109 +227,6 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
         }
     }
 
-
-    // FFMpeg Related stuff below
-
-
-    // matToBitmap converts an image Matrix to a Bitmap image.
-    private Bitmap matToBitmap(Mat mat){
-        Bitmap bitmap = Bitmap.createBitmap(mOpenCvCameraView.getWidth()/4,mOpenCvCameraView.getHeight()/4, Bitmap.Config.ARGB_8888);
-        try {
-            bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(mat, bitmap);
-
-        }catch(Exception e){
-            Log.e(TAG, "Exception converting mat to bitmap: " + e);
-        }
-        return bitmap;
-    }
-
-    // loadFFMpegBinary sets up Ffmpeg for doing commands.
-    private void loadFFMpegBinary() {
-        try {
-            ffmpeg.loadBinary(new LoadBinaryResponseHandler() {
-                @Override
-                public void onFailure() {
-
-                }
-            });
-        } catch (FFmpegNotSupportedException e) {
-            Log.e(TAG, "An exception happended setting up ffmpeg: " + e);
-        }
-    }
-
-    // execFFmpegBinary runs Ffppeg commands tokenized in a string array.
-    private void execFFmpegBinary(final String[] command) {
-        try {
-            ffmpeg.execute(command, new ExecuteBinaryResponseHandler() {
-                @Override
-                public void onFailure(String s) {
-                    Log.d(TAG, "FAILED with output : "+s);
-                }
-
-                @Override
-                public void onSuccess(String s) {
-                    Log.d(TAG, "SUCCESS with output : "+s);
-                }
-
-                @Override
-                public void onProgress(String s) {
-                    Log.d(TAG, "Progress command : ffmpeg " + Arrays.toString(command));
-                    Log.d(TAG, "progress : "+s);
-                }
-
-                @Override
-                public void onStart() {
-
-                    Log.d(TAG, "Started command : ffmpeg " + Arrays.toString(command));
-
-                }
-
-                @Override
-                public void onFinish() {
-                    Log.d(TAG, "Finished command : ffmpeg ");
-                    // TODO(paulmolloy): Figure out why it doesn't seem to be finished yet here.
-                }
-            });
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            Log.e(TAG, "Running ffmpeg: " + e);
-        }
-    }
-
-    // Saves a bitmap image to the applications dir with a given filename.
-    private void saveImage(Bitmap finalBitmap, String fileName) {
-
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root + VOT_DIR);
-        myDir.mkdirs();
-        String fname = fileName +".jpg";
-
-        File file = new File(myDir, fname);
-        if (file.exists()) file.delete ();
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-            Log.d(TAG, "Saved frame to: " + file.getAbsolutePath());
-
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to save frame: " + e);
-            e.printStackTrace();
-
-        }
-    }
-
-    // Saves a bitmap frame of the VOT using the frame number for file name.
-    public void saveTempBitmapFrame(Bitmap bitmap, int frameNum ) {
-        if (isExternalStorageWritable()) {
-            saveImage(bitmap, VOT_FRAME_PREFIX + frameNum);
-            Log.d(TAG, "Saved bitmap " + frameNum);
-        }else{
-            Log.w(TAG, "The external storage isn't writable, aborting saving frame.");
-        }
-    }
-
     /* Checks if external storage is available for read and write */
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
@@ -398,6 +254,7 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
         if (mMediaProjection == null) {
             // This asks for user permissions to capture the screen
             startActivityForResult(mProjectionManager.createScreenCaptureIntent(), CAST_PERMISSION_CODE);
+
             return;
         }
         mVirtualDisplay = createVirtualDisplay();
@@ -493,14 +350,12 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
 
     }
 
-
     // Firebase video stuff
 
     private void saveVotFirebase(String videoFilePath){
         // Save video to firebase
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
-        // TODO(paulmolloy): Save to per user dirs.
         StorageReference videoRef = storageRef.child(FIREBASE_VOT_DIR + Auth.getCurrentUserID() + FIREBASE_FILENAME + genCurDateString() + ".mp4");
 
         Uri file = Uri.fromFile(new File(videoFilePath)); //Declare your url here.
@@ -523,10 +378,6 @@ public class VotCamera extends Fragment implements CameraBridgeViewBase.CvCamera
                 Log.d(TAG, "Upload finished location: " + videoRef.getPath());
                 // TODO(paulmolloy): Save reference to it in Firestore
                 FSVotVideoRef.addVideoReference(videoRef.getPath(), "TB Vot on " + genCurDateString());
-
-
-
-
 
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
